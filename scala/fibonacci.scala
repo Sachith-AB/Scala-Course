0665a865b2ab6109dee1afc5f424841a04fbe706
Sachith Avintha <153298821+Sachith45@users.noreply.github.com>
2024-07-08 22:02:23 +0530

    submit labsheet 4

---
+++ scala/fibonacci.scala
@@ -0,0 +1,7 @@
+object Add{
+    def fibonacci(x:Int):Int{
+        if(x == 0){
+            return total
+        }else 
+    }
+}
---
+++ scala/fizzbuzz.scala
@@ -0,0 +1,17 @@
+object checkBuzz {
+    def checkBuzz(x:Int):Any ={
+        if(x%3==0 && x%5==0){
+            println("Fizzbuzz");
+        }else if(x%3 == 0){
+            println("Fizz");
+        }else{
+            println(I"$x");
+        }
+    }
+}
+
+object Main {
+    def main(args:Array[String]){
+        val number = 
+    }
+}
